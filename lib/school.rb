709f8cff47477e990139ecfe4c603306f24e83c7
Lenny Lee <lennylee91@gmail.com>
2019-12-24 07:47:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,13 +5,17 @@ class School
  
   attr_accessor :name, :roster
   
-   roster = []
+   @@roster = []
   def initialize
     @name=name
-    @roster = roster
+    @@roster << self
     
   end
   
+  def self.roster
+    
+  @@roster
+end
 
   
 end
